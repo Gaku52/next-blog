d076fe60d1cd71c4839afc10818ab0113b57e54b
Gaku52 <Gaku52@example.com>
2022-11-04 16:41:55 +0900

    update: 記事作成11/3,4分

---
+++ _posts/article-00012-Ubuntu.md
@@ -0,0 +1,27 @@
+---
+title: 'WindowsからLinuxへディレクトリを移動する方法'
+excerpt: 'cpコマンドで移動し、権限変更をしてディレクトリをLinux上で扱えるようにする。'
+coverImage: '/assets/blog/Ubuntu/Ubuntu.png'
+date: '2022-11-02'
+ogImage:
+  url: '/assets/blog/Ubuntu/Ubuntu.png'
+tags:
+  - 'Ubuntu'
+  - 'Linux'
+  - 'WSL2'
+---
+
+## 概要
+
+
+
+## 状況と原因
+
+
+## 解決方法
+
+
+
+### 参考サイト様
+
+
--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // hot reload
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 800, //チェック時間
-      aggregateTimeout: 300, // 遅延時間
-      ignored : ['node_modules']
-    };
-    return config;
-  },
   basePath: process.env.GITHUB_ACTIONS ? "/next-blog" : "",
   trailingSlash: true,
 };
